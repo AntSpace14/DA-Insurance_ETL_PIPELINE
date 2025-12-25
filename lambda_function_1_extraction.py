91c64f9037713a580b34a80a137b75cea6b94ad2
Antariksh Sarmah <spacer4nger05@gmail.com>
2025-12-25 21:38:26 +0530

    Change data source to HTTP and update S3 bucket
    Updated the lambda function to download insurance data from a public HTTP source instead of Kaggle, and modified the S3 bucket name.

--- lambda_function_1_extraction.py
+++ lambda_function_1_extraction.py
@@ -1,30 +1,31 @@
 import json
-import os
 import boto3
-import pandas as pd
+import urllib.request
 from datetime import datetime
-from kaggle.api.kaggle_api_extended import KaggleApi
 
 def lambda_handler(event, context):
+    # Download insurance data from HTTP
+    with urllib.request.urlopen("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv") as response:
+        csv_data = response.read().decode('utf-8')
     
-    # Initialize Kaggle API
-    api = KaggleApi()
-    api.authenticate()
+    # Convert CSV to JSON
+    lines = csv_data.strip().split('\n')
+    headers = lines[0].split(',')
     
-    # Download insurance dataset from Kaggle
-    dataset_name = "easonlai/sample-insurance-claim-prediction-dataset"
-    api.dataset_download_files(dataset_name, path='/tmp/', unzip=True)
-    
-    # Read CSV and convert to JSON
-    df = pd.read_csv('/tmp/insurance_claims.csv')
-    insurance_data = df.to_dict('records')
+    insurance_data = []
+    for line in lines[1:]:
+        values = line.split(',')
+        record = {}
+        for i, header in enumerate(headers):
+            record[header] = values[i]
+        insurance_data.append(record)
     
     # Upload to S3
     client = boto3.client('s3')
     filename = "insurance_raw_" + str(datetime.now()) + ".json"
     
     client.put_object(
-        Bucket="insurance-etl-pipeline-demo",
+        Bucket="insurance-etl-datalake-antariksh",  # UPDATE THIS
         Key="raw_data/to_processed/" + filename,
         Body=json.dumps(insurance_data)
     )
